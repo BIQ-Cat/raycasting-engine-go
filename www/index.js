1df571eebc674be9425641584ac2c3bf1e73d706
AnDev58 <andev58@outlook.com>
2025-04-16 18:54:10 +0300

    Added render function

--- www/index.js
+++ www/index.js
@@ -1,1 +1,36 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext('2d');
 
+const FPS = 60;
+const MS_PER_FRAME = 1000 / FPS;
+let frames = 0;
+
+setScreen(canvas.width, canvas.height);
+
+
+function renderMap() {
+  ctx.putImageData(getPixels(), 0, 0);
+}
+
+
+let msPrev = performance.now();
+function gameLoop() {
+  requestAnimationFrame(gameLoop);
+
+  const msNow = performance.now();
+  const msPassed = msNow - msPrev;
+
+  if (msPassed < MS_PER_FRAME) return;
+
+  renderMap();
+  
+  const excessTime = msPassed % MS_PER_FRAME;
+  msPrev = msNow - excessTime;
+
+  frames++
+}
+
+setInterval(() => { 
+  document.getElementById("fps").textContent = `FPS: ${frames}`;
+  frames = 0;
+}, 1000)
